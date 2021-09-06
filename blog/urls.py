7ec8ec12ea1c37ffeb84d968ebbe100d5ce39b8d
Guillermo Ramirez <prograx@mesoamericana.edu.gt>
2021-09-05 22:51:25 -0600

    Se creo la base y la plantilla Se agrego una plantilla con Bulma

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [    
+    path('', views.publicacion_lista, name='publicacion_lista'),
+    path('publicacion/<int:pk>', views.publicacion_detalle, name='publicacion_detalle'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
-# Create your views here.
+from .models import Publicacion
+
+def publicacion_lista(request):
+    publicaciones = Publicacion.objects.filter (fecha_publicacion__lte = timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/publicacion_listab.html', {'publicaciones': publicaciones})
+
+def publicacion_detalle (request, pk):
+    publicacion = get_object_or_404(Publicacion, pk = pk)
+    return render (request, 'blog/publicacion_detalle.html',{'publicacion':publicacion})
+
+
+
